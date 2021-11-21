a6607068a2249ca36566f052af3c40198fa6eed3
Jacob Moss <jacobmoss09@gmail.com>
2021-11-21 11:05:44 -0600

    prices dropdown

--- src/components/MenuStack.tsx
+++ src/components/MenuStack.tsx
@@ -4,40 +4,97 @@ import Stack from '@mui/material/Stack';
 import { styled } from '@mui/material/styles';
 import './MenuStack.css';
 import { Link } from 'react-router-dom';
-import { Typography } from '@mui/material';
+import {
+  Button,
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+} from '@mui/material';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import { extras, services } from './PricingPage';
 // import { Link } from './Link';
 
+const menuPages = [
+  {
+    pageName: 'Gallery',
+  },
+  {
+    pageName: 'Prices',
+  },
+  {
+    pageName: 'Booking',
+  },
+  {
+    pageName: 'Contact',
+  },
+];
+
 const MenuStack = React.forwardRef((props, ref) => {
-  const Item: any = styled(Paper)(({ theme }) => ({
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  }));
+  const [iconDown, setIconDown] = React.useState(false);
+
+  const handleOnClick = () => {
+    setIconDown(!iconDown);
+  };
 
   return (
-    <div className='menuStackWrapper'>
+    <div>
       <Stack spacing='10px' sx={{ marginTop: '10px' }} ref={ref}>
-        <Link to='gallery' className='menuItem'>
-          <Item>
-            <Typography variant='button'>Gallery</Typography>
-          </Item>
-        </Link>
-        <Link to='prices' className='menuItem'>
-          <Item>
-            <Typography variant='button'>Prices</Typography>
-          </Item>
-        </Link>
-        <Link to='booking' className='menuItem'>
-          <Item>
-            <Typography variant='button'>Booking</Typography>
-          </Item>
-        </Link>
-        <Link to='contact' className='menuItem'>
-          <Item>
-            <Typography variant='button'>Contact</Typography>
-          </Item>
-        </Link>
+        {menuPages.map(({ pageName }) => {
+          if (pageName === 'Prices') {
+            return (
+              <>
+                <Button
+                  variant='contained'
+                  endIcon={
+                    pageName === 'Prices' ? (
+                      <ArrowRightIcon
+                        className={iconDown ? 'iconDown' : 'iconRight'}
+                      />
+                    ) : null
+                  }
+                  sx={{ minWidth: '120px' }}
+                  onClick={pageName === 'Prices' ? handleOnClick : () => {}}
+                >
+                  <Typography variant='button'>{pageName}</Typography>
+                </Button>
+                {iconDown && (
+                  <List
+                    sx={{
+                      width: '100%',
+                      maxWidth: 360,
+                      bgcolor: 'background.paper',
+                      marginTop: '0 !important',
+                      marginBottom: '10px',
+                    }}
+                  >
+                    {services.map((service) => {
+                      return (
+                        <>
+                          <ListItem alignItems='flex-start'>
+                            <ListItemText secondary={service.name} />
+                          </ListItem>
+                          {service.name !== 'Ultimate Glam' ? (
+                            <Divider variant='middle' component='li' />
+                          ) : null}
+                        </>
+                      );
+                    })}
+                  </List>
+                )}
+              </>
+            );
+          } else {
+            return (
+              <Link to={pageName.toLowerCase()} className='menuItem'>
+                <Button variant='contained' sx={{ minWidth: '120px' }}>
+                  <Typography variant='button'>{pageName}</Typography>
+                </Button>
+              </Link>
+            );
+          }
+        })}
       </Stack>
     </div>
   );
