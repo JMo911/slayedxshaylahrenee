48aa0166b3ddf56ac732efbdadbda15489155ed8
Jacob Moss <jacobmoss09@gmail.com>
2022-02-08 20:19:42 -0600

    moved fake reviews to data directory

---
+++ src/data/reviews.ts
@@ -0,0 +1,32 @@
+export interface ReviewType {
+  rating: number;
+  comment: string;
+}
+
+export const fakeReviews: ReviewType[] = [
+  {
+    rating: 5,
+    comment:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    rating: 5,
+    comment:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    rating: 5,
+    comment:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    rating: 5,
+    comment:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    rating: 5,
+    comment:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+];
