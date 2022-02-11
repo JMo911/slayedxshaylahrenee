5711deb4be667194272c9c95dae25ed04f6d1590
Jacob Moss <jacobmoss09@gmail.com>
2022-02-10 21:46:40 -0600

    revert route work

--- src/index.tsx
+++ src/index.tsx
@@ -77,7 +77,6 @@ ReactDOM.render(
               <Route path='prices' element={<PricingPage />} />
               <Route path='booking' element={<BookingPage />} />
               <Route path='contact' element={<ContactPage />} />
-              <Route path='/' element={<WelcomePage />} />
             </Route>
           </Routes>
         </ServiceProvider>
